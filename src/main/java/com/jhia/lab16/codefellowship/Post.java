6ddbe1195026e8e34a7e55ebb359385c1693e2ab
Jhia Turner <jhimitu@gmail.com>
2019-06-20 12:16:43 -0700

    update post entity to have Id and Generated value

--- src/main/java/com/jhia/lab16/codefellowship/Post.java
+++ src/main/java/com/jhia/lab16/codefellowship/Post.java
@@ -2,10 +2,16 @@ package com.jhia.lab16.codefellowship;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
     String body;
     LocalDate createdAt;
 
